654dd85b72c70f892b19bf68db4b5c8852aea1c1
Michael Anderson <michael.anderson@elymentalz.com>
2008-08-02 17:04:27 +0930

    Fixed bug with 'saru-run-test suite .' creating .saru directory in wrong location

--- tests/runalltests.bash
+++ tests/runalltests.bash
@@ -1,6 +1,21 @@
 #!/bin/bash
 echo "Just a temporary wrapper script while we get saru able to take over..."
+
+#Old style named tests
+echo "====================="
+echo "  Old Style naming"
+echo "====================="
 for i in ??-??-* ; do
   echo -n $i 
   ./$i && echo " OK" || echo " FAILED" ; 
 done
+
+echo "====================="
+echo "  New Style naming"
+echo "====================="
+for i in ??-*-??-* ; do
+  echo -n $i
+  ./$i && echo " OK" || echo " FAILED";
+done
+
+
